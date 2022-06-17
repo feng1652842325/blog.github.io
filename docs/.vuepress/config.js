8381b44cc3ea97e8ef233e3873319c8625d691cd
sihangfeng <1652842325@qq.com>
2022-06-17 14:12:17 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  title: 'Sutera',
+  description: 'Sutera的个人Blog',
+
+  themeConfig: {
+    lastUpdated: '上一次更新',
+    nav: [
+      { text: '首页', link: '/' },
+      {
+        text: 'Sutera的个人博客',
+        items: [{ text: 'Github', link: 'https://github.com/mqyqingfeng' }]
+      }
+    ],
+    sidebar: [
+      {
+        title: '简介',
+        path: '/'
+      },
+      {
+        title: '前端三剑客',
+        collapsable: false, // 不折叠
+        children: [
+          {
+            title: 'JavaScript'
+          },
+          {
+            title: 'Css',
+            children: [{ title: 'clip-path', path: '/pages/Css/clipPath' }]
+          },
+          {
+            title: 'html'
+          }
+        ]
+      },
+      {
+        title: 'Vue'
+      },
+      {
+        title: '项目',
+        children: [{ title: '代码格式化及校验', path: '/pages/Settings/eslintFormat' }]
+      }
+    ]
+  },
+
+  markdown: {
+    lineNumbers: true // 显示行号
+  }
+};
